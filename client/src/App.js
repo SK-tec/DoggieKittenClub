79d81546a748eaa572360f070d4072fe71957ebe
samatha <samathagamidi@gmail.com>
2023-10-19 13:44:18 +0200

    Headersection added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import headerImage from "./images/HerosectionImage.jpg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome To Client</h1>
+      <div>
+        <img src={headerImage} alt="" className="col-12" />
+      </div>
+      <h1 className="heading m-4">
+        <span>Welcome To Doggie-Kitten Club</span>
+      </h1>
     </div>
   );
 }
